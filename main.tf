6cbd5b0262ad2fa288885b7b55d92974e0775b34
Abhijeet Kulkarni (Cloud AbhijeetKulkarni(Pla Platform Engineering) <abhijeet.kulkarni@intuitive.ai>
2026-02-28 00:43:34 +0530

    feat: use SSH module source for orgpolicy

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
 locals {
-  # Load all org-policy YAMLs dynamically
+  # Load only sw-unified-data-platform YAML files
   org_policy_config_files = fileset(
-    "${path.module}/config/org-policy",
-    "*/*.yaml"
+    "${path.module}/config/org-policy/sw-unified-data-platform",
+    "*.yaml"
   )
 
   org_policy_objects = [
     for f in local.org_policy_config_files :
-    yamldecode(file("${path.module}/config/org-policy/${f}"))
+    yamldecode(
+      file("${path.module}/config/org-policy/sw-unified-data-platform/${f}")
+    )
   ]
 }
 
 module "orgpolicy" {
-  # SSH module source (correct Terraform format)
   source = "git::ssh://git@github.com/AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//orgpolicy?ref=main"
 
   count = length(local.org_policy_objects) > 0 ? 1 : 0
