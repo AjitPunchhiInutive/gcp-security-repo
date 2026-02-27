ec0c6b2b57292f36503895685b5d88ba323f6e42
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-02-27 22:33:22 +0530

    Adding folder

--- main.tf
+++ main.tf
@@ -1,30 +1,12 @@
 locals {
-
-  # WIF: list from config/wif/*.yaml
-  wif_config_files = fileset("config/wif", "*.yaml")
-  wif_objects = [for f in local.wif_config_files : yamldecode(file("config/wif/${f}"))]
-
-  # Folders: single object from config/folders.yaml (module expects only organization_id, parent_folders, sub_folders)
-  folder_config_files = fileset("config/folders", "*.yaml")
-  folders_raw         = length(local.folder_config_files) > 0 ? yamldecode(file("config/folders/folders.yaml")) : null
-  folders_objects     = local.folders_raw != null ? { organization_id = local.folders_raw.organization_id, parent_folders = local.folders_raw.parent_folders, sub_folders = local.folders_raw.sub_folders } : { organization_id = var.organization_id, parent_folders = {}, sub_folders = {} }
-
-  
-  project_config_files = fileset("config/project-factory", "*.yaml")
-  project_objects = [for f in local.project_config_files : yamldecode(file("config/project-factory/${f}"))]
-}
-  # Projects: list from config/projects/*.yaml
-module "wif_factory" {
-  source                         = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//wif-factory?ref=main"
-  github_workload_identity_factory = local.wif_objects
+# Org-policy: single object from first file in config/org-policy/*.yaml
+  org_policy_config_files = fileset("config/org-policy/sw-unified-data-paltform", "*.yaml")
+  org_policy_objects = [for f in local.org_policy_config_files : yamldecode(file("config/org-policy/sw-unified-data-paltform${f}"))]
+  org_policies = length(local.org_policy_objects) > 0 ? local.org_policy_objects[0] : { deploy = false, folder_id = "",project_id = "", policy_boolean = {}, policy_list = {} }
 }
 
-  module "folders" {
-  source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//folders?ref=main"
-  folders_objects = local.folders_objects
+module "orgpolicy" {
+  source                         = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//orgpolicy?ref=main"
+  org_policies                   = local.org_policies
 }
 
-module "project-factory" {
-  source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//project-factory?ref=main"
-  project_objects = local.project_objects
-}
--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,4 @@
-output "wif_factory" {
-  description = "Folder module outputs"
-  value       = module.wif_factory
-}
-
-output "project-factory" {
-    description = "Project "
-    value = module.project-factory
-  
+output "orgpolicy" {
+  description = "org policy module outputs"
+  value       = module.orgpolicy
 }
