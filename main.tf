1ad07bd8c6c540809283d7e6563c68b4243aa7c2
Abhijeet Kulkarni (Cloud AbhijeetKulkarni(Pla Platform Engineering) <abhijeet.kulkarni@intuitive.ai>
2026-03-04 15:39:28 +0530

    Add org policies for sw-ent-security folder (15 policies)

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ locals {
   org_policy_config_files = [
     "sw-unified-data-platform/sw-unified-data-platform-folder.yaml",
     "sw-ent-networking/sw-ent-networking-folder.yaml",
+    "sw-ent-security/sw-ent-security-folder.yaml",
   ]
 
   active_org_policies = {
