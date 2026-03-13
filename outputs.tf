eded41bec12f023d448a01300f4839ab6b3cd73d
Abhijeet Kulkarni (Cloud AbhijeetKulkarni(Pla Platform Engineering) <abhijeet.kulkarni@intuitive.ai>
2026-03-13 17:42:32 +0530

    Fix outputs.tf to reference new module names

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "orgpolicy" {
-  description = "org policy module outputs"
-  value       = module.orgpolicy
+output "orgpolicy_udp" {
+  description = "org policy module outputs - unified data platform"
+  value       = module.orgpolicy_udp
+}
+
+output "orgpolicy_networking" {
+  description = "org policy module outputs - networking"
+  value       = module.orgpolicy_networking
+}
+
+output "orgpolicy_security" {
+  description = "org policy module outputs - security"
+  value       = module.orgpolicy_security
 }
